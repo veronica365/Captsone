0e015dc91956910c0de58f8b4538e6ad281a301c
veronica365 <naruth10@gmail.com>
2023-02-09 18:25:48 +0300

    Add changes to fix the linter errors found

--- assests/js/script.js
+++ assests/js/script.js
@@ -7,54 +7,50 @@ const $ = (element) => document.querySelector(element);
 const featuredSpeakers = [
   {
     id: 1,
-    name: "Suzan Wojiski",
-    jobDescription: "CEO of Youtube",
+    name: 'Suzan Wojiski',
+    jobDescription: 'CEO of Youtube',
     headline:
-      "Discover how to work more closely across your team with customer-led demos and templates.",
+      'Discover how to work more closely across your team with customer-led demos and templates.',
   },
   {
     id: 2,
-    name: "Larry Page",
-    jobDescription: "Google Co-founder &amp; Former CEO ",
+    name: 'Larry Page',
+    jobDescription: 'Google Co-founder &amp; Former CEO ',
     headline:
-      "Discover how to work more closely across your team with customer-led demos and templates.",
+      'Discover how to work more closely across your team with customer-led demos and templates.',
   },
   {
     id: 3,
-    name: "Catalina Popa",
-    jobDescription: "CEO of Creative Commons, ex COO of the Mozilla foundatio",
+    name: 'Catalina Popa',
+    jobDescription: 'CEO of Creative Commons, ex COO of the Mozilla foundatio',
     headline:
-      "Discover how to work more closely across your team with customer-led demos and templates.",
+      'Discover how to work more closely across your team with customer-led demos and templates.',
   },
   {
     id: 4,
-    name: "Ryan Merkley",
-    jobDescription: "CEO of Facebook",
+    name: 'Ryan Merkley',
+    jobDescription: 'CEO of Facebook',
     headline:
-      "Discover how to work more closely across your team with customer-led demos and templates.",
+      'Discover how to work more closely across your team with customer-led demos and templates.',
   },
   {
     id: 5,
-    name: "Daniel Oh",
-    jobDescription: "CEO of Apple",
+    name: 'Daniel Oh',
+    jobDescription: 'CEO of Apple',
     headline:
-      "Discover how to work more closely across your team with customer-led demos and templates.",
+      'Discover how to work more closely across your team with customer-led demos and templates.',
   },
   {
     id: 6,
-    name: "Johan Janssen",
-    jobDescription: "CEO of Tesla",
+    name: 'Johan Janssen',
+    jobDescription: 'CEO of Tesla',
     headline:
-      "Discover how to work more closely across your team with customer-led demos and templates.",
+      'Discover how to work more closely across your team with customer-led demos and templates.',
   },
 ];
-function showmore(){
-    $("#speakers").classList.toggle('mobile');
-    $(".show-more").classList.toggle('d-none');
-}
 
 const generateFeaturedSpeaker = (speakers) => {
-  let innerHTML = "";
+  let innerHTML = '';
   speakers.forEach((speaker) => {
     innerHTML += `<div class="col-lg-6 mb-4">
                 <div class="d-flex flex-row speaker card border-0">
@@ -72,14 +68,19 @@ const generateFeaturedSpeaker = (speakers) => {
   });
   const mobileViewMore = `
     <div class="col-12">
-        <div class="py-2 text-center d-block d-md-none border border-2 border-gray show-more" onclick={showmore()}>
+        <div class="py-2 text-center d-block d-md-none border border-2 border-gray show-more">
             <span class="m-0">More <i class="ms-2 fa-solid fa-angle-down text-danger fa-lg"></i></span>
         </div>
     </div>`;
-  return (innerHTML += mobileViewMore);
+  innerHTML += mobileViewMore;
+  return innerHTML;
 };
 
-const featuredTitle = $("#speakers .title-2");
-const featured = $("#speakers .speaker-wrapper .mx-auto");
-featuredTitle.innerHTML = `<h2 class="m-0 fw-bold">Featured Speakers</h2><hr />`;
+const featuredTitle = $('#speakers .title-2');
+const featured = $('#speakers .speaker-wrapper .mx-auto');
+featuredTitle.innerHTML = '<h2 class="m-0 fw-bold">Featured Speakers</h2><hr />';
 featured.innerHTML = generateFeaturedSpeaker(featuredSpeakers);
+$('.show-more').addEventListener('click', () => {
+  $('#speakers').classList.toggle('mobile');
+  $('.show-more').classList.toggle('d-none');
+});
